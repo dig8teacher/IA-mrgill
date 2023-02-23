97374c99e853d65da855b8bba57fde1c6972aa41
mr.gill <jgill@sd42.ca>
2023-02-23 11:09:51 -0800

    Update variableone.java

--- variableone.java
+++ variableone.java
@@ -4,7 +4,7 @@ class variableone {
 		int number1 = 22;
 		int number2 = 22;
 		int number3 = 22;
-		int number4 = 22
+		int number4 = 22;
 		System.out.println(number1+number2+number3+number4);
 	}
 }
