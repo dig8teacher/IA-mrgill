f6365e2b5096102b7fc3885da2bdb39d441b6a5b
mr.gill <jgill@sd42.ca>
2023-02-24 12:44:44 -0800

    Update addtwonumbten.java

--- addtwonumbten.java
+++ addtwonumbten.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
+
 
 public class addtwonumbten {
 
        public static void main(String[] args) {
 
        int num1 = 5, num2 = 5, sum;
-       System.out.println("please enter two numbers that total to 10");
-       Scanner sc = new Scanner(System.in);
-       System.out.println("enter 1st num");
-       num1 = sc.nextInt();
-
-       System.out.println("enter 2nd num");
-       num2 = sc.nextInt();
+       
        
        
        
